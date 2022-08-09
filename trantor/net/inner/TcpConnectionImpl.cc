86464ab9d21d0534d05080d978d27e5c3b066bd4
Bartek Kryza <bkryza@gmail.com>
2022-08-09 22:43:35 +0200

    Increased stream buffer size

--- trantor/net/inner/TcpConnectionImpl.cc
+++ trantor/net/inner/TcpConnectionImpl.cc
@@ -1593,6 +1593,8 @@ void TcpConnectionImpl::sendStream(
 
 void TcpConnectionImpl::sendFileInLoop(const BufferNodePtr &filePtr)
 {
+    const size_t kReadSize = 1 * 1024 * 1024;
+
     loop_->assertInLoopThread();
     assert(filePtr->isFile());
 #ifdef __linux__
@@ -1637,7 +1639,7 @@ void TcpConnectionImpl::sendFileInLoop(const BufferNodePtr &filePtr)
         if (!fileBufferPtr_)
         {
             fileBufferPtr_ = std::make_unique<std::vector<char>>();
-            fileBufferPtr_->reserve(16 * 1024);
+            fileBufferPtr_->reserve(kReadSize);
         }
         while ((filePtr->fileBytesToSend_ > 0) || !fileBufferPtr_->empty())
         {
@@ -1646,7 +1648,7 @@ void TcpConnectionImpl::sendFileInLoop(const BufferNodePtr &filePtr)
             {
                 //                LOG_TRACE << "send stream in loop: fetch data
                 //                on buffer empty";
-                fileBufferPtr_->resize(16 * 1024);
+                fileBufferPtr_->resize(kReadSize);
                 std::size_t nData;
                 nData = filePtr->streamCallback_(fileBufferPtr_->data(),
                                                  fileBufferPtr_->size());
@@ -1728,7 +1730,7 @@ void TcpConnectionImpl::sendFileInLoop(const BufferNodePtr &filePtr)
     LOG_TRACE << "send file in loop";
     if (!fileBufferPtr_)
     {
-        fileBufferPtr_ = std::make_unique<std::vector<char>>(16 * 1024);
+        fileBufferPtr_ = std::make_unique<std::vector<char>>(kReadSize);
     }
 #ifndef _WIN32
     lseek(filePtr->sendFd_, filePtr->offset_, SEEK_SET);
